33e13b11efcad3b03d19d135231ebb7545e924b0
David Maksimov <dmaksi@gmail.com>
2018-10-30 16:51:31 -0400

    Save response

--- src/Exceptions/ErrorResponseException.php
+++ src/Exceptions/ErrorResponseException.php
@@ -4,5 +4,24 @@ namespace Cmsmax\ZohoCrmApi\Exceptions;
 
 class ErrorResponseException extends \Exception
 {
+    protected $errorCode;
+    protected $response;
 
+    public function __construct($errorCode, $response)
+    {
+        $this->errorCode = $errorCode;
+        $this->response = $response;
+
+        parent::__construct("API returned error response: " . $this->errorCode);
+    }
+
+    public function getErrorCode()
+    {
+        return $this->errorCode;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
+    }
 }
